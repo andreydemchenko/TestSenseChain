5243e7e0fcaa63081dedca9a751083f28c436726
ademchenko <ademchenko@zuzex.com>
2022-12-09 17:28:39 +0300

    added pods files

---
+++ Main/Contracts/PostJob/ContractCreated/ContractCreatedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ContractCreatedViewController.swift
+//  TestSenseChain
+//
+//  Created by zuzex-62 on 09.12.2022.
+//
+
+import UIKit
+
+class ContractCreatedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction
+    private func pendingContractsBtnClicked(_ sender: Any) {
+ 
+    }
+    
+    @IBAction
+    private func backToContractsBtnClicked(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Contracts", bundle: nil)
+        if let vc = storyboard.instantiateViewController(withIdentifier: "ContractsVC") as? ContractsViewController {
+            vc.presenter = ContractsPresenter(view: vc, service: appContext.authentication)
+            vc.hidesBottomBarWhenPushed = true
+            vc.modalPresentationStyle = .fullScreen
+            navigationController?.popToRootViewController(animated: false)
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
